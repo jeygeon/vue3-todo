b3ac8776a26261a90031c1b8b8428ebe58748d91
crinity-jaeygun <jaeygun@crinity.com>
2025-03-31 17:15:05 +0900

    commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-text-field label="First name" variant="underlined"></v-text-field>
+    </v-container>
+  </v-app>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+// src/plugins/vuetify.js
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  theme: {
+    defaultTheme: "light",
+    //
+  },
+});
+
+export default vuetify;
